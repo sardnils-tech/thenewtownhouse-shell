206c62f1c4640f1f18deb841cd9746ca37023c0a
alexis nilo la hoz sarduy <your.email@example.com>
2025-10-04 06:18:30 -0600

    Initial commit of thenewtownhouse shell

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl">React + Vite + Tailwind CSS</h1>
+    </div>
+  );
+}
+
+export default App;
